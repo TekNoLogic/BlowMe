f1698086eb71cdc4da28c5239fc973b0a06806bb
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-10-28 01:55:18 -0600

    Make button show and hide

--- frames/button.lua
+++ frames/button.lua
@@ -6,6 +6,7 @@ local butt = CreateFrame("Button", nil, UIParent, "SecureActionButtonTemplate")
 butt:SetAttribute("type*", "item")
 butt:SetAttribute("item*", "item:141605")
 butt:SetSize(48, 48)
+butt:Hide()
 
 
 local icon = butt:CreateTexture()
@@ -20,7 +21,25 @@ local function OnEnter(self)
 end
 
 
+local function Show()
+  if InCombatLockdown() then return C_Timer.After(1, Show) end
+  butt:Show()
+end
+
+
+local function Hide()
+  if InCombatLockdown() then return C_Timer.After(1, Hide) end
+  butt:Hide()
+end
+
+
+local function OnShow(self)
+  C_Timer.After(3*60, Hide)
+end
+
 ns.MakeRepositionable(butt)
 
 butt:SetScript("OnEnter", OnEnter)
 butt:SetScript("OnLeave", GameTooltip_Hide)
+butt:SetScript("OnShow", OnShow)
+ns.RegisterCallback(butt, "_WORLD_QUEST_COMPLETED", Show)
